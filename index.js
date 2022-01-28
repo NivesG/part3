72fdc900181a8d1c2133fa7954d5890dbc23492a
NivesG <nives.gabrijan@gmail.com>
2022-01-28 18:39:33 +0100

    phonebook backend 3.3

--- index.js
+++ index.js
@@ -37,9 +37,16 @@ app.get('/api/persons', (req, res) => {
 app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has ${persons.length} people</p>
               <p>${new Date().toString()}</p>`)
+    })
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {res.json(person)}
+    else {res.status(404).end()}
     
 })
-
 
 const PORT = 3003
 app.listen(PORT)
