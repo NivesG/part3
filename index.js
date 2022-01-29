5d6fbbbcc6c63faa4c589c3625e6d894fb09a682
NivesG <nives.gabrijan@gmail.com>
2022-01-29 13:36:11 +0100

    phonebook 3.7

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const http = require('http')
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postReq' ))
+
+morgan.token('postReq', function(req) {
+  if(req.method === 'POST')
+  return JSON.stringify(req.body)
+});
+
+
+const logger = morgan('tiny')
 
 let persons = [
     { 
@@ -73,7 +83,6 @@ app.post('/api/persons', (req, res) => {
     if (persons.some(n => n.name === body.name)) {
         return res.status(400).json({error: 'name must be unique'})
     }
-    
 
     const person = {
         name: body.name,
